17ddecab63d0d7e577d21577e9f37ab65922362a
raehanramadhanhamzah <raehanramadh27@gmail.com>
2023-10-05 19:45:40 +0800

    Revisi Tugas Praktikum 4

--- Tugas Praktikum 4/Nomor_2/main.js
+++ Tugas Praktikum 4/Nomor_2/main.js
@@ -1,30 +1,41 @@
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
 const arrAlphabet = alphabet.split("")
 let stringKosong = ""
-let text = "Indonesia"
+let text = "Indonesia Raya"
 let pisahText = text.toLowerCase().split("")
+let pisahTextAsli = text.split("")
 let n = 13
 let sementara = 0
 let putaran = 0
 for (let i = 0; i < text.length; i++) {
-    let batasUjung = arrAlphabet.indexOf(pisahText[sementara]) + n
-    if (batasUjung > 25) {
-        let hitungPutaran = parseInt((arrAlphabet.indexOf(pisahText[sementara]) + n) / 26)
-        putaran = hitungPutaran
-        let hasilPutaran = 26 * putaran
-        let hasil = arrAlphabet.indexOf(pisahText[sementara]) + n - hasilPutaran
-        stringKosong += arrAlphabet[hasil]
-        sementara += 1
+    if (arrAlphabet.includes(pisahText[sementara])) {
+        let batasUjung = arrAlphabet.indexOf(pisahText[sementara]) + n
+        if (batasUjung > 25) {
+            let hitungPutaran = parseInt((arrAlphabet.indexOf(pisahText[sementara]) + n) / 26)
+            putaran = hitungPutaran
+            let hasilPutaran = 26 * putaran
+            let hasil = arrAlphabet.indexOf(pisahText[sementara]) + n - hasilPutaran
+            if (pisahTextAsli[sementara] === pisahTextAsli[sementara].toUpperCase()) {
+                stringKosong += arrAlphabet[hasil].toUpperCase()
+            } else {
+                stringKosong += arrAlphabet[hasil]
+            }
+            sementara += 1
 
+        } else {
+            let indexHuruf = arrAlphabet.indexOf(pisahText[sementara])
+            if (pisahTextAsli[sementara] === pisahTextAsli[sementara].toUpperCase()) {
+                stringKosong += arrAlphabet[indexHuruf + n].toUpperCase()
+            } else {
+                stringKosong += arrAlphabet[indexHuruf + n]
+            }
+            sementara += 1
+        }
     } else {
-        let indexHuruf = arrAlphabet.indexOf(pisahText[sementara])
-        stringKosong += arrAlphabet[indexHuruf + n]
+        stringKosong += " "
         sementara += 1
     }
 }
 
-if(text.charAt(0) === text.charAt(0).toUpperCase()){
-    console.log(stringKosong.charAt(0).toUpperCase()+ stringKosong.substring(1))
-}else{
-    console.log(stringKosong)
-}
+console.log(stringKosong)
+
