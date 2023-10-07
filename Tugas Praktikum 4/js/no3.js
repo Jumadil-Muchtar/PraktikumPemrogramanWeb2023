737f34dc7dd6c38ab15b5d6f3c6455cbdd8c945d
Kyyneko <mahendrakirana284@gmail.com>
2023-10-07 22:22:23 +0800

    H071221058_Mahendra Kirana M.B

--- Tugas Praktikum 4/js/no3.js
+++ Tugas Praktikum 4/js/no3.js
@@ -1,9 +1,9 @@
 let kata = prompt("Masukkan Kata : ");
 let kataLowerCase = kata.toLowerCase();
-let kataAwal = kataLowerCase.split('');
+let kataAwal = kataLowerCase.split("");
 let kataAkhir = kataAwal.reverse().slice(); // Menggunakan slice() untuk menghindari perubahan array asli
 
-kataAkhir = kataAkhir.join('');
+kataAkhir = kataAkhir.join("");
 
 if (kataLowerCase == kataAkhir) {
   alert(`${kata} adalah Palindrom`);
--- Tugas Praktikum 4/js/no4.js
+++ Tugas Praktikum 4/js/no4.js
@@ -2,30 +2,29 @@ function main() {
   const n = parseInt(prompt("Masukkan jumlah angka: "));
   const angka = [];
 
-if (!isNaN(n)) {
-  for (let i = 0; i < n; i++) {
-    const number = parseInt(prompt(`Masukkan angka ke-${i + 1}: `));
-    angka.push(number);
-  }
+  if (!isNaN(n)) {
+    for (let i = 0; i < n; i++) {
+      const number = parseInt(prompt(`Masukkan angka ke-${i + 1}: `));
+      angka.push(number);
+    }
 
-  // Melakukan pengurutan menggunakan Bubble Sort
-  for (let i = 0; i < angka.length - 1; i++) {
-    for (let j = 0; j < angka.length - i - 1; j++) {
-      if (angka[j] > angka[j + 1]) {
-        // Tukar angka[j] dan angka[j+1]
-        const temp = angka[j];
-        angka[j] = angka[j + 1];
-        angka[j + 1] = temp;
+    // Melakukan pengurutan menggunakan Bubble Sort
+    for (let i = 0; i < angka.length - 1; i++) {
+      for (let j = 0; j < angka.length - i - 1; j++) {
+        if (angka[j] > angka[j + 1]) {
+          // Tukar angka[j] dan angka[j+1]
+          const temp = angka[j];
+          angka[j] = angka[j + 1];
+          angka[j + 1] = temp;
+        }
       }
     }
+
+    const sortedAngka = "Angka yang telah diurutkan :\n" + angka.join(" ");
+    alert(sortedAngka);
+  } else {
+    alert("Jumlah Angka Harus Berupa Integer");
   }
-  
-  const sortedAngka = "Angka yang telah diurutkan :\n" + angka.join(" ");
-  alert(sortedAngka);
-  
-} else {
-  alert("Jumlah Angka Harus Berupa Integer")
-}
 }
 
 main();
